a9f79c73a202066d8aeef3f8a01b2907098ade7f
Nathan Doucette <fluffyandnathan@gmail.com>
2023-04-14 17:44:11 -0400

    update

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,17 @@
 use yew::prelude::*;
 
-struct Model{
-    value: i32
-}
 
 #[function_component(App)]
 fn app() -> Html {
-    let state = use_state(|| Model {
-        value: 0
-    });
-
-    let onclick = {
-        let state = state.clone();
-
-        Callback::from(move |_| {
-            state.set(Model {
-                value: state.value + 1
-            })
-        })
-    };
-
     html! {
         <form>
-            <TextInput name="username">
-            <label for="lifts">Choose a lift:</label>
+            <label>
+                <input type="text" name="username"/>
+            </label>
             <select name="lifts" id="lift select">
-                <option value="Bench">Bench</option>
-                <option value="Squat">Squat</option>
-                <option value="Deadlift">Deadlift</option>
+                <option value="Bench">{"Bench"}</option>
+                <option value="Squat">{"Squat"}</option>
+                <option value="Deadlift">{"Deadlift"}</option>
             </select>
         </form>
     }
